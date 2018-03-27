5fb6871388df4955f3bb86f497344b5f2fe5e0d9
Soundappan Nagalingam <n.soundappan@gmail.com>
2018-03-27 16:14:53 -0600

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,46 @@
+﻿import {VAL99, VAL2, VAL3, VAL98} from "./JavaScript";
+
+let showMessage = function () { alert("Hey! Its Me !!") };
+showMessage();
+
+let myObject = { account: "1234", branch: "aurora" };
+let myObject1 = {account: "1234", name:"Hello"};
+let myObject4 = {address: "12 as", name:"HiThere", type:"saving"};
+
+let objMap = Object.assign(myObject, myObject1, myObject4);
+
+console.log(myObject.account);
+console.log(objMap);
+
+let mySetObject = new Set();
+mySetObject.add(VAL99);
+mySetObject.add(VAL98);
+mySetObject.add(VAL2);
+mySetObject.add(VAL3);
+
+console.log(mySetObject);
+
+let string = "Its the Sum !!";
+let [first, second, ...rest] = mySetObject;
+
+let finalResult = returnIt("of", addMe(rest));
+
+console.log(finalResult);
+
+function returnIt(str, strRet){
+    return ("Here is the total " + str + " " + strRet);
+}
+
+function addMe(setArr){
+    let total = 0;
+    for (let arr of setArr)
+    {
+        total += arr;
+    }
+    return (total);
+}
+
+
+let objClass = new subClass("Class JavaScript", "ECMA");
+console.log(objClass.showName());
+console.log(objClass.showAddress());
---
+++ classFile.js
@@ -0,0 +1,28 @@
+﻿export const VAL99 = 99;
+export  const VAL98 = 98;
+export  const VAL2 = 2;
+export  const VAL3 = 3;
+
+class baseClass {
+    constructor(name, address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    showName() {
+        return (this.name);
+    }
+}
+
+class subClass extends baseClass {
+    super(name, address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    showAddress() {
+        return (this.address);
+    }
+}
+
+ 
